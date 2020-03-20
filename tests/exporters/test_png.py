439ce9fd1e9825d7d582a7bf20302ff3bdb88339
Andreas Arnesson <andreas_osby@hotmail.com>
2020-03-20 16:15:07 +0100

    tests for png exporter and cleaned code for exporter

--- tests/exporters/test_png.py
+++ tests/exporters/test_png.py
@@ -9,10 +9,83 @@ from treeviz.exporters import png
 class TestPngExporter(unittest.TestCase):
     """Submodule for unittests, derives from unittest.TestCase"""
 
-    @mock.patch("treeviz.exporters.png.os.system")
-    def test_create_png(self, os_mock):
-        png.create_png()
-        
+    @mock.patch("treeviz.exporters.png.platform")
+    def test_create_cmd_wsl(self, platform_mock):
+        """
+        test the create_command function with wsl platform
+        """
+        platform_mock.platform.return_value = "Linux-4.4.0-17763-Microsoft-x86_64-with-Ubuntu-18.04-bionic"
+        dotfile = "tree.dot"
+        pngfile = "tree.png"
+        dir_path = "/c/Users/Zeldah/git/treeviz"
+
+        cmd = png.create_cmd(dotfile, pngfile, dir_path)
+        self.assertEqual(
+            cmd,
+            "powershell.exe dot.exe -Tpng C:/Users/Zeldah/git/treeviz/tree.dot -o C:/Users/Zeldah/git/treeviz/tree.png"
+        )
+
+
+
+    @mock.patch("treeviz.exporters.png.platform")
+    def test_create_cmd_mac(self, platform_mock):
+        """
+        test the create_command function with mac platform
+        """
+        platform_mock.platform.return_value = "Darwin-18.7.0-x86_64-i386-64bit"
+        dotfile = "tree.dot"
+        pngfile = "tree.png"
+        dir_path = "/home/zeldah/git/treeviz"
+
+        cmd = png.create_cmd(dotfile, pngfile, dir_path)
+        self.assertEqual(cmd, "dot -Tpng /home/zeldah/git/treeviz/tree.dot -o /home/zeldah/git/treeviz/tree.png")
+
+
+
+    @mock.patch("treeviz.exporters.png.platform")
+    def test_create_cmd_linux(self, platform_mock):
+        """
+        test the create_command function with linux platform
+        """
+        platform_mock.platform.return_value = "Linux-4.19.84-standard-x86_64-with-debian-10.0"
+        dotfile = "tree.dot"
+        pngfile = "tree.png"
+        dir_path = "/home/zeldah/git/treeviz"
+
+        cmd = png.create_cmd(dotfile, pngfile, dir_path)
+        self.assertEqual(cmd, "dot -Tpng /home/zeldah/git/treeviz/tree.dot -o /home/zeldah/git/treeviz/tree.png")
+
+
+
+    @mock.patch("treeviz.exporters.png.wpc")
+    def test_create_wsl_command_c_path(self, wpc_m):
+        """
+        test the create_wsl_command function with /c/ path for dir
+        """
+        expected_path = "C:/Users/Zeldah/git/treeviz"
+        wpc_m.convert_m.return_value = expected_path
+        dot_cmd = "powershell.exe dot.exe"
+
+        windows_path_c = "/c/Users/Zeldah/git/treeviz"
+        dot_and_dir = (dot_cmd, expected_path)
+        self.assertEqual(dot_and_dir, png.create_wsl_command(windows_path_c))
+        wpc_m.convert_m.assert_called_once_with(windows_path_c)
+
+
+
+    @mock.patch("treeviz.exporters.png.wpc")
+    def test_create_wsl_command_mnt_path(self, wpc_m):
+        """
+        test the create_wsl_command function with /mnt/ path for dir
+        """
+        expected_path = "C:/Users/Zeldah/git/treeviz"
+        wpc_m.convert_m.return_value = expected_path
+        dot_cmd = "powershell.exe dot.exe"
+
+        windows_path = "/mnt/c/Users/Zeldah/git/treeviz"
+        dot_and_dir = (dot_cmd, expected_path)
+        self.assertEqual(dot_and_dir, png.create_wsl_command(windows_path))
+        wpc_m.convert_m.assert_called_once_with(windows_path)
 
 
 
--- treeviz/exporters/png.py
+++ treeviz/exporters/png.py
@@ -5,55 +5,48 @@ Needs to have Graphviz installed and in path.
 import os
 import re
 import platform
+import wsl_path_converter as wpc
 
 def create_png(dotfile="tree.dot", pngfile="tree.png"):
     """
-    Use powershell to convert dot file to png using Graphviz
+    Convert dot file to png using Graphviz
     """
     dir_path = os.path.dirname(os.path.realpath(__name__))
     cmd = create_cmd(dotfile, pngfile, dir_path)
-    print(cmd)
     os.system(cmd)
 
 
 
 def create_cmd(dotfile, pngfile, dir_path):
-    info = platform.platform().lower()
-    cmd = "{dot} -Tpng {dir}/{dotfile} -o {dir}/{pngfile}"
+    """
+    Create terminal command for creating png of dot file.
+    """
+    cmd_str = "{dot} -Tpng {dir}/{dotfile} -o {dir}/{pngfile}"
+    dot = "dot"
 
+    info = platform.platform().lower()
     if "microsoft" in info and "linux" in info:
-        cmd = create_wsl_command(cmd, dir_path)
-    elif "cygwin" in info:
-        # cant install igraph on cygwin
-        pass
+        dot, dir_path = create_wsl_command(dir_path)
     elif "darwin" in info or "linux" in info:
-        cmd = cmd.format(
-            dot="dot",
-            dir=dir_path,
-            dotfile=dotfile,
-            pngfile=pngfile,
-        )
+        pass
     else:
         raise OSError("Your OS is not supported!")
-    return cmd
 
-
-
-def create_wsl_command(cmd, dir_path):
-    # kan använda powershell.exe start ... för att starta ett program
-    # powershell.exe start $(wslpath -w $(which dot.exe))
-    # wslpath har fler options...
-    cmd.format(
-        dot="dot.exe",
-        dir=fix_c_in_windows_path(dir_path),
+    cmd = cmd_str.format(
+        dot=dot,
+        dir=dir_path,
+        dotfile=dotfile,
+        pngfile=pngfile,
     )
-    cmd = "powershell.exe 'dot.exe " + cmd + ";exit"
     return cmd
 
-def fix_c_in_windows_path(path):
+
+
+def create_wsl_command(dir_path):
     """
-    Fix wsl path in windows to work in powershell.
+    Use wsl_path_converter to convert wsl format path to windows
+    and return powershell command for graphviz/dot.
     """
-    if path.startswith("/c/"):
-        path = path.replace("/c/", "C:/")
-    return path
+    dot = "powershell.exe dot.exe"
+    dir = wpc.convert_m(dir_path)
+    return dot, dir
