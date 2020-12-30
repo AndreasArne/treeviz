5de95dcc71093383412d595c9e44793e8dad93e5
Andreas Arnesson <andreas_osby@hotmail.com>
2020-12-30 23:35:50 +0100

    reads dot config from file

--- treeviz/exporters/dot.py
+++ treeviz/exporters/dot.py
@@ -1,6 +1,9 @@
 """
 Exports graph to dot file.
 """
+import json
+
+config_filename = ".dot.json"
 
 def to_dot(graph, filename="tree.dot"):
     """
@@ -20,27 +23,32 @@ def write_to_file(content, filename):
 
 
 
+def create_config(graph_type, filename=config_filename):
+    """
+    Create config string for dot file graph.
+    """
+    with open(filename) as fd:
+        config_json = json.load(fd)
+
+    config = ""
+    for graph_part, values in config_json[graph_type].items():
+        options = [f"  {key}={value}" for key, value in values.items()]
+        config += graph_part + " [\n" + ",\n".join(options) + "\n];\n"
+    return config
+
+
+
 def build_string(graph):
     """
     Create string with dot code from graph
     """
-    graph_settings = """digraph {
-  graph[
-    nodesep=0.25, ranksep=0.3, splines=line
-  ];
-  node [
-    style=filled, fillcolor=lightblue,
-    shape=circle, fixedsize=true, width=0.5
-  ];
-  edge [
-    arrowsize=0.8
-  ];
-  """
+    graph_settings = create_config(graph.__class__.__name__)
     nodes = "\n".join([repr(vertex) for vertex in graph.vertexes.values()])
     edges = "\n".join([repr(edge) for edge in graph.edges])
 
-    dot_string = "{start}\n{nodes}\n{edges}\n}}".format(
-        start=graph_settings,
+    dot_string = "{graph_type} {{\n{config}\n{nodes}\n{edges}\n}}".format(
+        graph_type=graph._graph_type,
+        config=graph_settings,
         nodes=nodes,
         edges=edges,
     )
