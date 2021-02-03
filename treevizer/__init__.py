41b80aa43adcee3968fcafa8e008dff6661b9236
Andreas Arnesson <andreas_osby@hotmail.com>
2021-02-03 21:14:50 +0100

    (docs) Upped to version 0.0.5

--- treevizer/__init__.py
+++ treevizer/__init__.py
@@ -4,7 +4,7 @@ Builds the public api with __all__.
 """
 from treevizer.main import tree_to_dot, tree_to_png, dot_to_png
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 # what is __all__ https://stackoverflow.com/a/35710527
 __all__ = [
