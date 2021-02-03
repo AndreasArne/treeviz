1aa2774e59b883b0eca00df444ae35d8ebb4d6f9
Andreas Arnesson <andreas_osby@hotmail.com>
2021-02-03 20:57:03 +0100

    (test) Integrations test no longer verify images
    Can't find a way to compate images cross plattform.
    Now we compare dot files instead.

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -13,29 +13,38 @@ class TestIntegration(unittest.TestCase):
     """Submodule for unittests, derives from unittest.TestCase"""
     
     
-    def test_tree_to_png_bbt(self):
+    @mock.patch("treevizer.builders.vertex.id")
+    def test_tree_to_png_bbt(self, id_mock):
         """
         Test full run of tree to png for bbt
         """
+        def side_effect(name):
+            return name
+        id_mock.side_effect = side_effect
+
         bst = Bst()
         utils.list_to_bst([10, 20, 0, 33, 2,  1, 15], bst)
         
         with tempfile.TemporaryDirectory(dir="./") as tmpdirname:
             treevizer.tree_to_png(bst.root, "bbt", tmpdirname+"/tree.dot", tmpdirname+"/tree.png")
-            with open(tmpdirname+"/tree.png", "rb") as test_png:
-                with open("tests/fixtures/integration_files/bst.png", "rb") as correct_png:
-                    self.assertEqual(test_png.read(), correct_png.read())
+            with open(tmpdirname+"/tree.dot", "rb") as test_dot:
+                with open("tests/fixtures/integration_files/bst.dot", "rb") as correct_dot:
+                    self.assertEqual(test_dot.read(), correct_dot.read())
 
 
-
-    def test_tree_to_png_ll(self):
+    @mock.patch("treevizer.builders.vertex.id")
+    def test_tree_to_png_ll(self, id_mock):
         """
         Test full run of tree to png for ll
         """
+        def side_effect(name):
+            return name
+        id_mock.side_effect = side_effect
+
         ll = Node(10, Node(20, Node("0", Node(33, Node(2,  Node(1, Node("test")))))))
         
         with tempfile.TemporaryDirectory(dir="./") as tmpdirname:
             treevizer.tree_to_png(ll, "ll", tmpdirname+"/tree.dot", tmpdirname+"/tree.png")
-            with open(tmpdirname+"/tree.png", "rb") as test_png:
-                with open("tests/fixtures/integration_files/ll.png", "rb") as correct_png:
-                    self.assertEqual(test_png.read(), correct_png.read())
+            with open(tmpdirname+"/tree.dot", "rb") as test_dot:
+                with open("tests/fixtures/integration_files/ll.dot", "rb") as correct_dot:
+                    self.assertEqual(test_dot.read(), correct_dot.read())
