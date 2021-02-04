c567f7498cba3d46e7d4c31d31fce4d4ef47d168
Andreas Arnesson <andreas_osby@hotmail.com>
2021-02-04 13:45:33 +0100

    (style) Move comment to fix validation error

--- treevizer/__init__.py
+++ treevizer/__init__.py
@@ -1,10 +1,7 @@
 """
 Make treevizer a package.
 Builds the public api with __all__.
-"""
-from treevizer.main import tree_to_dot, tree_to_png, dot_to_png
 
-"""
 Follows semantic versioning, https://semver.org/spec/v2.0.0.html
 Given a version number MAJOR.MINOR.PATCH, increment the:
 
@@ -12,6 +9,8 @@ Given a version number MAJOR.MINOR.PATCH, increment the:
     MINOR version when you add functionality in a backwards compatible manner, and
     PATCH version when you make backwards compatible bug fixes.
 """
+from treevizer.main import tree_to_dot, tree_to_png, dot_to_png
+
 
 __version__ = "0.0.6"
 
